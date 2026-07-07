9e62cd86b6a10609b03f9719c10266238962fecb
Sathish Kumar B <sathishsince2004@gmail.com>
2026-07-07 23:10:00 +0530

    Update SecurityConfig.java

--- src/main/java/Sathish292004/config/SecurityConfig.java
+++ src/main/java/Sathish292004/config/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -113,6 +118,35 @@ public class SecurityConfig {
                         jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class
                 );
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+        
+            CorsConfiguration configuration = new CorsConfiguration();
+        
+            configuration.setAllowedOrigins(List.of(
+                    "http://localhost:5173",
+                    "https://sk-store-drab.vercel.app"
+            ));
+        
+            configuration.setAllowedMethods(List.of(
+                    "GET",
+                    "POST",
+                    "PUT",
+                    "DELETE",
+                    "OPTIONS"
+            ));
+        
+            configuration.setAllowedHeaders(List.of("*"));
+        
+            configuration.setAllowCredentials(true);
+        
+            UrlBasedCorsConfigurationSource source =
+                    new UrlBasedCorsConfigurationSource();
+        
+            source.registerCorsConfiguration("/**", configuration);
+        
+            return source;
+        }
 
         return http.build();
     }
