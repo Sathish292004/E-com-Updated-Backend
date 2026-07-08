fb1c7aa0c9cb57e43cd4caa0b12b0734730a345b
Sathish Kumar B <sathishsince2004@gmail.com>
2026-07-08 18:37:25 +0530

    Refactor Category class with Lombok annotations

--- src/main/java/Sathish292004/model/Category.java
+++ src/main/java/Sathish292004/model/Category.java
@@ -1,19 +1,19 @@
 package Sathish292004.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Column(nullable = false, unique = true)
     private String name;
